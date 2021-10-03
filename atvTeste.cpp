460dc7ed83be0bf0c7c530ae7e3bbae5d8a03423
Natan Holanda <natanholanda@alu.ufc.br>
2021-10-03 19:03:27 -0300

    Primeira atividade teste

---
+++ atvTeste.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+ cout << "Soma de dois números!\n Digite o primeiro número :\n";
+int n1, n2;
+  cin >> n1;
+  cout << "O segundo número : "; cin >> n2;
+  cout << "\nA soma = "<< n1 + n2;
+
+return 0;
+}
